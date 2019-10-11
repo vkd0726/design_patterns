a241a7a32c07deb5bba26f748f02d02dc117655b
yonycherkos <yonycherkos@gmail.com>
2019-10-11 14:56:01 +0300

    Create and implement the DeleteContactCommand class

--- behavioral_pattern/command_pattern/SharingApp/app/src/main/java/com/example/sharingapp/DeleteContactCommand.java
+++ behavioral_pattern/command_pattern/SharingApp/app/src/main/java/com/example/sharingapp/DeleteContactCommand.java
@@ -1,4 +1,23 @@
 package com.example.sharingapp;
 
-public class DeleteContactCommand {
+import android.content.Context;
+
+/**
+ * Command to delete an item
+ */
+public class DeleteContactCommand extends Command {
+    private ContactList contact_list;
+    private Contact contact;
+    private Context context;
+
+    public DeleteContactCommand(ContactList contact_list, Contact contact, Context context) {
+        this.contact_list = contact_list;
+        this.contact = contact;
+        this.context = context;
+    }
+
+    public void execute() {
+        contact_list.deleteContact(contact);
+        setIsExecuted(contact_list.saveContacts(context));
+    }
 }
