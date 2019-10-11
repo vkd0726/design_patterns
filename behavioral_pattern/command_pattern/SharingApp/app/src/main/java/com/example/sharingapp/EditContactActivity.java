3bb86ea3bf306cc2f5ee1e5f77ac22ea9dd70de2
yonycherkos <yonycherkos@gmail.com>
2019-10-11 15:17:50 +0300

    Update EditContactActivity to use DeleteContactCommand and EditContactCommand

--- behavioral_pattern/command_pattern/SharingApp/app/src/main/java/com/example/sharingapp/EditContactActivity.java
+++ behavioral_pattern/command_pattern/SharingApp/app/src/main/java/com/example/sharingapp/EditContactActivity.java
@@ -66,9 +66,14 @@ public class EditContactActivity extends AppCompatActivity {
 
         Contact updated_contact = new Contact(username_str, email_str, id);
 
-        contact_list.deleteContact(contact);
-        contact_list.addContact(updated_contact);
-        contact_list.saveContacts(context);
+        // Edit contact
+        EditContactCommand edit_contact_command = new EditContactCommand(contact_list, contact, updated_contact, context);
+        edit_contact_command.execute();
+
+        boolean success = edit_contact_command.isExecuted();
+        if(!success){
+            return;
+        }
 
         // End EditContactActivity
         finish();
@@ -76,8 +81,14 @@ public class EditContactActivity extends AppCompatActivity {
 
     public void deleteContact(View view) {
 
-        contact_list.deleteContact(contact);
-        contact_list.saveContacts(context);
+        // Delete contact
+        DeleteContactCommand delete_contact_command = new DeleteContactCommand(contact_list, contact, context);
+        delete_contact_command.execute();
+
+        boolean success = delete_contact_command.isExecuted();
+        if(!success){
+            return;
+        }
 
         // End EditContactActivity
         finish();
