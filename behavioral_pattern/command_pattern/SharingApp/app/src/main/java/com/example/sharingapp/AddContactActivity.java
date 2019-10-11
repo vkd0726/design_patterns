93d53df7900605a4ca2f02d1446aa86f9810e4f4
yonycherkos <yonycherkos@gmail.com>
2019-10-11 15:07:40 +0300

    Update AddContactActivity to use AddContactCommand

--- behavioral_pattern/command_pattern/SharingApp/app/src/main/java/com/example/sharingapp/AddContactActivity.java
+++ behavioral_pattern/command_pattern/SharingApp/app/src/main/java/com/example/sharingapp/AddContactActivity.java
@@ -56,8 +56,14 @@ public class AddContactActivity extends AppCompatActivity {
 
         Contact contact = new Contact(username_str, email_str, null);
 
-        contact_list.addContact(contact);
-        contact_list.saveContacts(context);
+        // Add contact
+        AddContactCommand add_contact_command = new AddContactCommand(contact_list, contact, context);
+        add_contact_command.execute();
+
+        boolean success = add_contact_command.isExecuted();
+        if(!success){
+            return;
+        }
 
         // End AddContactActivity
         finish();
